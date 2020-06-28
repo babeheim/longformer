58fbba133c0dfde26ec61ac48f373fa658f35baa
Bret Beheim <beheim@gmail.com>
2020-06-28 12:48:54 +0200

    added generate_schema function

--- R/database_functions.R
+++ R/database_functions.R
@@ -18,3 +18,39 @@ load_database <- function(path = "data/") {
   return(db)
 
 }
+
+
+generate_schema <- function(db, keys, sensitive_variables, verbose = TRUE) {
+
+    # generate a list of tables
+    tables <- data.frame(
+        table = names(db),
+        nobs = unlist(lapply(db, nrow)),
+        nvar = unlist(lapply(db, ncol)),
+        stringsAsFactors = FALSE
+    )
+
+    # generate a list of variables
+    for(i in 1:nrow(tables)){
+        add <- data.frame(variable=colnames(db[[i]]))
+        add$variable <- as.character(add$variable)
+        add$table <- tables$table[i]
+        add$class <- unlist(lapply(db[[i]], class))
+        add$n_present <- colSums(!is.na(db[[i]]) & db[[i]]!='')
+        add$n_missing <- colSums(is.na(db[[i]]) | db[[i]]=='')
+        if (i == 1) {
+            variables <- add
+        } else {
+          variables <- bind_rows(variables, add)
+        }
+        if (verbose) print(tables$table[i])
+    }
+    variables$primary_key <- as.numeric(paste(variables$table, variables$variable) %in%
+     paste(keys$table, keys$variable))
+
+    variables$confidential <- as.numeric(variables$variable %in% sensitive_variables)
+
+    output <- list(variables = variables, tables = tables)
+    return(output)
+
+}
